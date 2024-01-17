783d8df814a072ab58316a2cce70b486e9334616
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2024-01-17 21:41:17 +0600

    update

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-
+const isOpen = ref(false);
 </script>
 
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <div class="flex items-center space-x-3 rtl:space-x-reverse">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Nuxt 3</span>
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+      <div class="flex items-center space-x-3">
+        <UAvatar size="md" src="https://www.datocms-assets.com/205/1667561117-nuxt.svg" />
+        <div class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Nuxt 3</div>
       </div>
-      <div>
+      <div class="flex gap-2">
+        <UButton icon="i-ph-magnifying-glass-bold" @click="isOpen = true" size="lg" color="gray" variant="ghost" />
+        <UModal v-model="isOpen">
+          <UCommandPalette multiple nullable />
+        </UModal>
+        <UButton icon="i-ph-bell-bold" size="lg" color="gray" variant="ghost" />
         <NuxtLink to="/login">
-          <UButton label="Login" />
+          <UButton label="Login" size="lg" />
         </NuxtLink>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,9 @@
   <NavigationBar />
   <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
     <div class="flex flex-col lg:grid lg:grid-cols-10 lg:gap-8">
-      <div class="lg:col-span-2 h-screen bg-red-200"></div>
+      <div class="lg:col-span-2 h-screen bg-red-200 hidden lg:block">
+        
+      </div>
       <div class="lg:col-span-8 h-screen bg-green-200"></div>
     </div>
   </div>
